2b7792fd12d0f656f429adfa2bb93183805b481c
Michal Lopuszynski <lopusz@users.noreply.github.com>
2016-09-15 21:03:07 +0200

    Importing the amsteRdam package

---
+++ src/featureHashing.cpp
@@ -0,0 +1,70 @@
+#include <string>
+#include <iostream>
+
+#include <stdlib.h>
+#include <Rcpp.h>
+
+#include "MurmurHash3.h"
+
+using namespace std;
+using namespace Rcpp;
+
+int calcHash(const char *s, int max , uint32_t seed)  {
+	const void *p=(const void *)s;
+    int hashingRes;
+    MurmurHash3_x86_32(p,strlen(s),seed,&hashingRes);
+	int res=(abs(hashingRes) % max)+1;
+	return hashingRes>0 ? res : -res;
+}
+
+//' Hashes the data frame \code{df}, so the output contains \code{numCols}
+//'
+//' @param df data frame to be hashed
+//' @param numCols integer, number of columns for the output data frame
+//' @param hashSeed integer, seed for the employed hash function (MurmurHash3)
+//' @export
+// [[Rcpp::export]]
+DataFrame hashDataFrame(DataFrame df, int numCols,int hashSeed) {
+    // Create empty numCols dataFrame and fill it with zeros
+
+    int nrows=df.nrows();
+    List resList(numCols);
+    for(int i=0; i<numCols; i++) {
+        resList[i]=NumericVector(nrows);
+    }
+    CharacterVector colNames(numCols);
+
+    for(int i=0; i<numCols; i++) {
+    	stringstream ss;
+        ss << "H" <<  (i+1);
+        colNames[i]=ss.str();
+    }
+
+    // Loop over columns, add substract each column depending on hash
+
+    CharacterVector cnames=df.attr("names");
+  	for(CharacterVector::iterator iter=cnames.begin(); iter!=cnames.end(); iter++) {
+  		int type=0;
+  		int h=calcHash(*iter, numCols, hashSeed);
+  		int targetCol=abs(h);
+  		double sign=h<0 ? -1.0 : 1.0;
+  		string s=as<string>(*iter);
+
+      if (TYPEOF(df[s])==REALSXP) {
+  			NumericVector x=df[s];
+      	for(int i=0; i<nrows; i++) {
+  		    as<NumericVector>(resList[targetCol-1])[i]+=sign*x[i];
+  	    }
+      }
+      else if (TYPEOF(df[s])==INTSXP) {
+      	IntegerVector x=df[s];
+      	for(int i=0; i<nrows; i++) {
+      		as<NumericVector>(resList[targetCol-1])[i]+=sign*x[i];
+    	}
+    }
+	}
+
+  DataFrame result(resList);
+  result.attr("names") = colNames;
+  return result;
+}
