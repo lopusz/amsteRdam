7d10a7b5ddf8dd4ae47333dd79931f3a0264dcf4
Michal Lopuszynski <lopusz@users.noreply.github.com>
2016-09-19 17:59:18 +0200

    Adding NA handling

--- src/amseRdam.cpp
+++ src/amseRdam.cpp
@@ -10,11 +10,19 @@ using namespace std;
 using namespace Rcpp;
 
 int calcHash(const char *s, int max , uint32_t seed)  {
-	const void *p=(const void *)s;
+    const void *p=(const void *)s;
     int hashingRes;
     MurmurHash3_x86_32(p,strlen(s),seed,&hashingRes);
-	int res=(abs(hashingRes) % max)+1;
-	return hashingRes>0 ? res : -res;
+    int res=(abs(hashingRes) % max)+1;
+    return hashingRes>0 ? res : -res;
+}
+
+inline bool isNotDoubleNA(double x) {
+  return x==x;
+}
+
+inline bool isNotIntNA(int x) {
+  return x!=NA_INTEGER;
 }
 
 //' Hashes the data frame \code{df}, so the output contains \code{numCols}
@@ -35,7 +43,7 @@ DataFrame hashDataFrame(DataFrame df, int numCols, int hashSeed) {
     CharacterVector colNames(numCols);
 
     for(int i=0; i<numCols; i++) {
-    	stringstream ss;
+        stringstream ss;
         ss << "H" <<  (i+1);
         colNames[i]=ss.str();
     }
@@ -43,26 +51,30 @@ DataFrame hashDataFrame(DataFrame df, int numCols, int hashSeed) {
     // Loop over columns, add substract each column depending on hash
 
     CharacterVector cnames=df.attr("names");
-  	for(CharacterVector::iterator iter=cnames.begin(); iter!=cnames.end(); iter++) {
-  		int type=0;
-  		int h=calcHash(*iter, numCols, hashSeed);
-  		int targetCol=abs(h);
-  		double sign=h<0 ? -1.0 : 1.0;
-  		string s=as<string>(*iter);
+    for(CharacterVector::iterator iter=cnames.begin(); iter!=cnames.end(); iter++) {
+        int type=0;
+        int h=calcHash(*iter, numCols, hashSeed);
+        int targetCol=abs(h);
+        double sign=h<0 ? -1.0 : 1.0;
+        string s=as<string>(*iter);
 
       if (TYPEOF(df[s])==REALSXP) {
-  			NumericVector x=df[s];
-      	for(int i=0; i<nrows; i++) {
-  		    as<NumericVector>(resList[targetCol-1])[i]+=sign*x[i];
-  	    }
+            NumericVector x=df[s];
+        for(int i=0; i<nrows; i++) {
+          if (isNotDoubleNA(x[i])) {
+            as<NumericVector>(resList[targetCol-1])[i]+=sign*x[i];
+            }
+        }
       }
       else if (TYPEOF(df[s])==INTSXP) {
-      	IntegerVector x=df[s];
-      	for(int i=0; i<nrows; i++) {
-      		as<NumericVector>(resList[targetCol-1])[i]+=sign*x[i];
-    	}
+        IntegerVector x=df[s];
+        for(int i=0; i<nrows; i++) {
+          if (isNotIntNA(x[i])) {
+             as<NumericVector>(resList[targetCol-1])[i]+=sign*x[i];
+            }
+        }
     }
-	}
+    }
 
   DataFrame result(resList);
   result.attr("names") = colNames;
--- tests/testthat/test-hashDataFrame.R
+++ tests/testthat/test-hashDataFrame.R
@@ -8,3 +8,18 @@ dfOut<-data.frame(H1=c(-2,-3,-4),H2=c(1,1,1),H3=c(-5,-7,-9))
 test_that("Basic sanity check of data frame hashing on simple example", {
   expect_equal(hashDataFrame(dfInp,3,0),dfOut)
 })
+
+dfInpNADouble<-dfInp
+dfInpNADouble$col7<-c(NA,NA,NA)
+dfInpNADouble$col8<-c(NA,NA,NA)
+dfInpNADouble$col9<-c(NA,NA,NA)
+
+dfInpNAInt<-dfInp
+dfInpNAInt$col7<-as.integer(c(NA,NA,NA))
+dfInpNAInt$col8<-as.integer(c(NA,NA,NA))
+dfInpNAInt$col9<-as.integer(c(NA,NA,NA))
+
+test_that("Sanity check for missing values (NA)", {
+  expect_equal(hashDataFrame(dfInpNADouble,3,0),dfOut)
+  expect_equal(hashDataFrame(dfInpNAInt,3,0),dfOut)
+})
